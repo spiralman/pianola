62031f334eeba21bb69378179350678eb4ed5c56
Thomas Stephens <spiralman@gmail.com>
2017-09-16 15:20:28 -0500

    First sketch of adding rhythm to the music

--- src/pianola/core.clj
+++ src/pianola/core.clj
@@ -5,6 +5,15 @@
             [leipzig.scale :as scale])
   (:use [overtone.inst.piano]))
 
+(defmethod live/play-note :default [{midi :pitch seconds :duration}]
+  (piano midi seconds :decay 0.001))
+
+(defn play-tune [tune]
+  (->>
+   tune
+   (tempo (bpm 50))
+   (where :pitch (comp scale/C scale/major))
+   live/play))
 
 (defn neighborhood [gen i v]
   [(nth gen (dec i) (last gen))
@@ -21,27 +30,36 @@
 
 (def automaton [1 2 0 4 5 6 3])
 
+(def rhythm [4 8 4 8 2 4 4 4])
 (def notes [2 1 2 1 2 3 4 5])
 
 (def run-automaton (partial next-gen automaton))
+
+(defn scale-rhythm [gen]
+  (map #(if (= 0 %)
+          0
+          (/ 1 %)) gen))
 
 (next-gen notes)
 
 (run-automaton [1 1 1 1 1 4 4 4])
 
+(automate-rhythm [1 1 1 1 1 4 4 4])
+
+(automate-rhythm rhythm)
+
 (take 5 (iterate run-automaton notes))
 
 (def melody
-  (phrase (repeat 1/4)
+  (phrase rhythm
+          notes))
+
+(def melodic-automaton
+  (phrase (flatten (iterate automate-rhythm rhythm))
           (flatten (iterate run-automaton notes))))
 
-(defmethod live/play-note :default [{midi :pitch seconds :duration}]
-  (piano midi seconds :decay 0.001))
+(play-tune melody)
 
-(->>
- melody
- (tempo (bpm 70))
- (where :pitch (comp scale/C scale/major))
- live/play)
+(play-tune melodic-automaton)
 
 (live/stop)
