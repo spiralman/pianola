37a150dc58b2dcf1e72240b7dd5f937153089777
Thomas Stephens <spiralman@gmail.com>
2017-09-24 21:01:32 -0500

    More setup for editing

--- src/pianola/notation.cljs
+++ src/pianola/notation.cljs
@@ -44,15 +44,52 @@
          (> y (.-y box))
          (< y (+ (.-y box) (.-h box))))))
 
-(defn handle-click [notes e]
-  (let [target (.-currentTarget e)
-        x (- (.-pageX e) (.-offsetLeft target))
-        y (- (.-pageY e) (.-offsetTop target))]
-    (println (first (drop-while #(not (within-note? x y %)) notes)))))
+(defn target-pos [e]
+  (let [target (.-currentTarget e)]
+    [(- (.-pageX e) (.-offsetLeft target))
+     (- (.-pageY e) (.-offsetTop target))]))
+
+(defn partition-edit [notes e]
+  (let [[x y] (target-pos e)
+        [head tail] (split-with #(not (within-note? x y %)) notes)
+        editing (first tail)
+        taking (second tail)
+        tail (drop 2 tail)]
+    [head editing taking tail]))
+
+(defn stretch [x editing taking]
+  (println "stretching" x)
+  [editing taking])
+
+(defn shift [y editing]
+  (println "shifting" y)
+  editing)
+
+(defn shift-or-stretch [dx dy x y editing taking]
+  (if (> dx dy)
+    (stretch x editing taking)
+    [(shift y editing) taking]))
+
+(defn abs [n] (max n (- n)))
+
+(defn edit-selected [{:keys [start partition]} [x y]]
+  (let [[head editing taking tail] partition
+        [start-x start-y] start
+        dx (abs (- x start-x))
+        dy (abs (- y start-y))
+        [editing taking] (shift-or-stretch dx dy x y editing taking)]
+    (apply flatten head editing taking tail)))
 
 (defn notation [phrase]
   (let [locals (r/atom {:context nil
-                        :notes []})]
+                        :notes []
+                        :editing nil})
+        start-edit #(swap! locals assoc :editing {:start (target-pos %)
+                                                  :partition (partition-edit (:notes @locals) %)})
+        edit (fn [e]
+               (if-let [editing (:editing @locals)]
+                 (swap! locals assoc :notes (edit-selected editing (target-pos e)))))
+        end-edit #(swap! locals assoc :editing nil)]
     (r/create-class
      {:display-name "notation"
       :component-did-mount
@@ -67,4 +104,6 @@
           (swap! locals assoc :notes (render-phrase context (:music (r/props this))))))
       :reagent-render
       (fn [phrase]
-        [:div {:onClick #(handle-click (:notes @locals) %)}])})))
+        [:div {:onMouseDown start-edit
+               :onMouseUp end-edit
+               :onMouseMove edit}])})))
